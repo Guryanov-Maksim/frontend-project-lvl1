300929d76df2577335cc8c1a97b85f6fbcbd5dcf
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-02-07 11:49:19 +0300

    refactor: change code after changing of the function interface in cli.js
    rus: изменение кода после изменения интерфейса функции в cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,4 +2,4 @@
 import getUserName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-console.log(`Hello, ${getUserName()}`);
+console.log(`Hello, ${getUserName('May I have your name? ')}`);
