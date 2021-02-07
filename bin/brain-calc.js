28df95567dc2c78a18348fac2a5333bd0046ce4c
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-02-07 16:49:34 +0300

    feat: add a new game named brain-calc
    rus: добавляет новую игру brain-calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import runGame, { getRandomNumber, getRandomMathSign } from '../src/index.js';
+
+const getQuestion = () => {
+  const num1 = getRandomNumber(1, 50);
+  const num2 = getRandomNumber(1, 50);
+  const sign = getRandomMathSign();
+  return `${num1} ${sign} ${num2}`;
+};
+
+const getCorrectAnswer = (mathExpression) => {
+  const [num1, sign, num2] = mathExpression.split(' ');
+  if (sign === '+') {
+    return `${Number(num1) + Number(num2)}`;
+  }
+  if (sign === '-') {
+    return `${Number(num1) - Number(num2)}`;
+  }
+  return `${Number(num1) * Number(num2)}`;
+};
+
+const isWrongAnswer = (playerAnswer, correctAnswer) => playerAnswer !== correctAnswer;
+
+runGame(getQuestion, getCorrectAnswer, isWrongAnswer);
