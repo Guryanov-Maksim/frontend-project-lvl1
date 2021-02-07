54f1992c9a0e6bb6a81f67865a350b6c5f2c131d
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-02-07 18:32:29 +0300

    fix: correct the task for a player
    rus: корректирует задание для игрока

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,10 +2,12 @@
 
 import runGame, { getRandomNumber, isEven } from '../src/index.js';
 
+const task = 'Answer "yes" if the number is even, otherwise answer "no".';
+
 const getQuestion = () => getRandomNumber(1, 50);
 
 const getCorrectAnswer = (num) => (isEven(num) ? 'yes' : 'no');
 
 const isWrongAnswer = (playerAnswer, correctAnswer) => playerAnswer !== correctAnswer;
 
-runGame(getQuestion, getCorrectAnswer, isWrongAnswer);
+runGame(getQuestion, getCorrectAnswer, isWrongAnswer, task);
