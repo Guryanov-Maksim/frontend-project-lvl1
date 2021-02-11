1f5883b90155db7283a68e031114d9cdef91e013
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-02-11 15:58:25 +0300

    refactor: exclude magic numbers
    rus: исключает магические числа

--- games/prime.js
+++ games/prime.js
@@ -1,17 +1,20 @@
 export default (num) => {
 // Primality test using 6k+-1 optimization
-  if (num <= 3) {
+  const firstPrimeInListOfPrimes = 2;
+  const secondPrimeInListOfPrimes = 3;
+  const modulus = 6;
+  if (num <= secondPrimeInListOfPrimes) {
     return num > 1;
   }
-  if (num % 2 === 0 || num % 3 === 0) {
+  if (num % firstPrimeInListOfPrimes === 0 || num % secondPrimeInListOfPrimes === 0) {
     return false;
   }
-  let i = 5;
-  while (i ** 2 <= num) {
-    if (num % i === 0 || num % (i + 2) === 0) {
+  let primeOfForm = 5; // primeOfForm is an prime of the form 6k+-1
+  while (primeOfForm ** 2 <= num) {
+    if (num % primeOfForm === 0 || num % (primeOfForm + 2) === 0) {
       return false;
     }
-    i += 6;
+    primeOfForm += modulus;
   }
   return true;
 };
