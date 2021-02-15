571ee49dc424a0161e8f42f265328e9e0a11d5e3
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-02-15 16:36:09 +0300

    refactor: hide details of realization
    rus: скрывает детали реализации

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
 import runGame from '../src/index.js';
-import isEven from '../games/even.js';
-import getRandomNumber from '../src/randomGenerator.js';
+import { getOneRandomNumber, checkEvenness, getTaskForPlayer } from '../src/games/even.js';
 
 runGame(
-  () => getRandomNumber(1, 50),
-  (num) => (isEven(num) ? 'yes' : 'no'),
-  'Answer "yes" if the number is even, otherwise answer "no".',
+  getOneRandomNumber,
+  checkEvenness,
+  getTaskForPlayer,
 );
--- src/games/even.js
+++ src/games/even.js
@@ -1,1 +1,9 @@
-export default (num) => num % 2 === 0;
+import getRandomNumber from '../randomNumber.js';
+
+const isEven = (num) => num % 2 === 0;
+
+export const getOneRandomNumber = () => getRandomNumber(1, 50);
+
+export const checkEvenness = (num) => (isEven(num) ? 'yes' : 'no');
+
+export const getTaskForPlayer = () => 'Answer "yes" if the number is even, otherwise answer "no".';
