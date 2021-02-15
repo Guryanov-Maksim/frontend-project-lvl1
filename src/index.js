e57bca5fc2f42f421651f0cce641b3e045b62a67
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-02-15 16:34:39 +0300

    refactor: change API
    rus: изменяет API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import getUserAnswer from './cli.js';
 
-export default (getQuestion, getCorrectAnswer, task) => {
+const toString = (data) => {
+  if (Array.isArray(data)) {
+    return data.join(' ');
+  }
+  return String(data);
+};
+
+export default (getQuestion, getCorrectAnswer, getTaskForPlayer) => {
   console.log('Welcome to the Brain Games!');
   const name = getUserAnswer('May I have your name? ');
   console.log(`Hello, ${name}`);
-  console.log(task);
-  for (let round = 1; round <= 3; round += 1) {
+  const taskForPlayer = getTaskForPlayer();
+  console.log(taskForPlayer);
+  for (let round = 0; round < 3; round += 1) {
     const question = getQuestion();
-    console.log(`Question: ${question}`);
+    console.log(`Question: ${toString(question)}`);
     const answer = getUserAnswer('Your answer: ');
     const correctAnswer = getCorrectAnswer(question);
-    if (answer !== `${correctAnswer}`) {
+    if (answer !== toString(correctAnswer)) {
       console.log(`'${answer}' is wrong answer ;( Correct answer was '${correctAnswer}'`);
       console.log(`Let's try again, ${name}!`);
       return;
