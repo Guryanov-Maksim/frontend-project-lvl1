860105ff0ef7dee68ca04b9435360dcef0ec60ae
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-02-15 16:03:04 +0300

    refactor: delete unnecessary type conversion
    rus: удаляет ненужное преобразование типов

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,4 +1,4 @@
-import getRandomNumber from '../src/randomGenerator.js';
+import getRandomNumber from '../randomNumber.js';
 
 const getRandomMathSign = () => {
   const signs = ['+', '-', '*'];
@@ -10,18 +10,18 @@ export const getMathExpression = () => {
   const num1 = getRandomNumber(1, 50);
   const num2 = getRandomNumber(1, 50);
   const sign = getRandomMathSign();
-  return `${num1} ${sign} ${num2}`;
+  return [num1, sign, num2];
 };
 
 export const solveMathExpression = (mathExpression) => {
-  const [num1, sign, num2] = mathExpression.split(' ');
+  const [num1, sign, num2] = mathExpression;
   if (sign === '+') {
-    return `${Number(num1) + Number(num2)}`;
+    return num1 + num2;
   }
   if (sign === '-') {
-    return `${Number(num1) - Number(num2)}`;
+    return num1 - num2;
   }
-  return `${Number(num1) * Number(num2)}`;
+  return num1 * num2;
 };
 
 export const getTaskForPlayer = () => 'What is the result of the expression?';
