1d6b785604f1b1bc3c20b69dd9a129b2d5443dcc
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-02-16 12:12:49 +0300

    refactor: simplify premality test
    rus: упрощает проверку на простоту числа

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,20 +1,13 @@
-export default (num) => {
-// Primality test using 6k+-1 optimization
-  const firstPrimeInListOfPrimes = 2;
-  const secondPrimeInListOfPrimes = 3;
-  const modulus = 6;
-  if (num <= secondPrimeInListOfPrimes) {
-    return num > 1;
-  }
-  if (num % firstPrimeInListOfPrimes === 0 || num % secondPrimeInListOfPrimes === 0) {
-    return false;
-  }
-  let primeOfForm = 5; // primeOfForm is an prime of the form 6k+-1
-  while (primeOfForm ** 2 <= num) {
-    if (num % primeOfForm === 0 || num % (primeOfForm + 2) === 0) {
+import getRandomNumber from '../randomNumber.js';
+
+const isPrime = (num) => {
+  let devisor = 2;
+  while (devisor < Math.sqrt(num)) {
+    const remainder = num % devisor;
+    if (remainder === 0) {
       return false;
     }
-    primeOfForm += modulus;
+    devisor += 1;
   }
   return true;
 };
